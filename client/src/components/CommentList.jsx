09fc88111242e9c872dc2e27e2d65b24f6dc6453
didof <didonato.fr@gmail.com>
2021-01-23 17:07:21 +0100

    client sync with backend

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from 'react'
 
+function applyModerationOnComment(comment, status) {
+    switch (status) {
+        case 'approved':
+            return <b>{comment}</b>
+        case 'pending':
+            return <span>{comment}</span>
+        case 'rejected':
+            return <i>This comment is been moderated</i>
+    }
+}
+
 function CommentList({ comments }) {
-    
+
     function renderComments() {
-        return comments.map(function({ id, content }) {
+        return comments.map(function ({ id, content, status }) {
+
+
             return (
                 <li
                     key={id}
                 >
-                    {content}
+                    {applyModerationOnComment(content, status)}
                 </li>
             )
         })
