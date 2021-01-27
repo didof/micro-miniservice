96e73b139b63b0e33cd251a6dfdeda7791be8a4c
didof <didonato.fr@gmail.com>
2021-01-27 22:33:43 +0100

    completed ingress configuration

--- client/src/components/PostCreate.jsx
+++ client/src/components/PostCreate.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const url = 'http://posts.com/posts'
+const url = 'http://posts.com/posts/create'
 
 function PostCreate() {
     const [title, setTitle] = useState('')
